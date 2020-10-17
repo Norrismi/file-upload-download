5136b813207938e553c42371e0da60e66799ab8d
Norrismi <Norrismi0@gmail.com>
2020-10-17 14:43:37 -0400

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className='m-3'>
+      <h1>Firegram</h1>
+      <h2 className='d-flex justify-content-center'>Your Pictures</h2>
+      <p className='d-flex justify-content-center m-5'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+        neque dolorem dolore voluptas error vitae. Ipsam vitae eligendi id non
+        alias, quisquam aliquid, reprehenderit temporibus, quos vel accusantium
+        odio quidem!
+      </p>
+    </div>
+  );
+};
+
+export default Title;
